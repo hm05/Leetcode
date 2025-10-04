68e1ddc89fdd73161998faf05d809d4fc3af3482
hm05 <harshmurjani5114@gmail.com>
2025-10-04 16:57:50 +0530

    Problem number - 2114

---
+++ Super Easy Problems/2114.MaximumWordsInSentence.cpp
@@ -0,0 +1,46 @@
+/*
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+Return the maximum number of words that appear in a single sentence.
+
+Example 1:
+Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+Output: 6
+Explanation: 
+- The first sentence, "alice and bob love leetcode", has 5 words in total.
+- The second sentence, "i think so too", has 4 words in total.
+- The third sentence, "this is great thanks very much", has 6 words in total.
+Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+Example 2:
+Input: sentences = ["please wait", "continue to fight", "continue to win"]
+Output: 3
+Explanation: It is possible that multiple sentences contain the same number of words. 
+In this example, the second and third sentences (underlined) have the same number of words.
+*/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentence){
+        int maxWord = 0;
+        for(string sen : sentence){
+            int words = 1;
+            for(char alphabet : sen){
+                if(alphabet == ' ') words++;
+            }
+            if(words > maxWord) maxWord = words;
+        }
+        return maxWord;
+    }
+};
+int main(){
+    Solution sol;
+    vector<string> sentence = {"please wait", "continue to fight", "continue to win"};
+    cout << sol.mostWordsFound(sentence);
+    return 0;
+}
+
+// Expected answer - 3
