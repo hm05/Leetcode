c1fb33aef9382b9048864881b12f3d1d1305a137
hm05 <harshmurjani5114@gmail.com>
2026-01-05 17:05:05 +0530

    1975

---
+++ Practice/1975.MaximumMatrixSum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long totalSum = 0;
+        int negCount = 0;
+        int minAbs = INT_MAX;
+
+        for (auto &row : matrix) {
+            for (int val : row) {
+                if (val < 0) negCount++;
+                totalSum += abs(val);
+                minAbs = min(minAbs, abs(val));
+            }
+        }
+
+        if (negCount % 2 == 1) {
+            totalSum -= 2LL * minAbs;
+        }
+
+        return totalSum;
+    }
+};
+
+// Status - Accepted
